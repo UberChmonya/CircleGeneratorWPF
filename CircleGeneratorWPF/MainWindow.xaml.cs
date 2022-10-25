1271181fbe61d1cc121fec33d6557541243a9bf0
Chmonya <100andrey300@gmail.com>
2022-10-26 01:47:58 +0700

    work delete

--- CircleGeneratorWPF/MainWindow.xaml.cs
+++ CircleGeneratorWPF/MainWindow.xaml.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CircleGeneratorWPF
 {
     public partial class MainWindow : Window
     {
-        private int _pixelSize;
+
         private readonly Circle _circle;
         double aspect = 1.3;
         public MainWindow()
@@ -18,40 +22,41 @@ namespace CircleGeneratorWPF
 
             canvas = new Canvas();
             _circle = new Circle(40, 10, 10);
-            _pixelSize = 1;
             InitializeComponent();
         }
-
-        private void Slider_Size(object sender, RoutedPropertyChangedEventArgs<double> slider)
-        {
-            _pixelSize = (int)slider.NewValue;
-            _circle.ChangeSize(_pixelSize);
-            DrawCircle();
-        }
-        private void Slider_Radius(object sender, RoutedPropertyChangedEventArgs<double> slider)
-        {
-            var radius = (int)slider.NewValue;
-            _circle.changeRadius(radius);
-            DrawCircle();
-        }
-
-        
-        private void DrawCircle()
+    
+        private void DrawCircle(int pixelSize)
         {
             canvas.Children.Clear();
             foreach (var point in _circle.Points)
             {
                 canvas.Children.Add(point.Rect);
-                Canvas.SetLeft(point.Rect, point.X * _pixelSize - _pixelSize / 2);
-                Canvas.SetTop(point.Rect, point.Y * _pixelSize - _pixelSize / 2);
+                Canvas.SetLeft(point.Rect, point.X * pixelSize - pixelSize / 2);
+                Canvas.SetTop(point.Rect, point.Y * pixelSize - pixelSize / 2);
             }
         }
         protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
         {
             if (sizeInfo.WidthChanged) this.Width = sizeInfo.NewSize.Height * aspect;
             else this.Height = sizeInfo.NewSize.Width / aspect;
-
+            
         }
-    }
+        private void RadiusPreview(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsValid(((TextBox)sender).Text + e.Text);
+        }
+        private void RadiusChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            int radius = string.IsNullOrEmpty(textBox.Text) ? 1 : int.Parse(textBox.Text);
+            _circle.changeRadius(radius);
+            DrawCircle(1);
+        }
+        private static bool IsValid(string str)
+        {
+            int i;
+            return int.TryParse(str, out i) && i >= 1 && i <= 300;
+        }
 
+    }
 }
