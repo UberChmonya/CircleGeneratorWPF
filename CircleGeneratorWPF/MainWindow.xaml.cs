4da61b27b0cc9e5628335a6c035336c859afb615
sad_degenerate <mister.franz02@gmail.com>
2022-10-26 13:47:48 +0700

    Created function to find canvas size.

--- CircleGeneratorWPF/MainWindow.xaml.cs
+++ CircleGeneratorWPF/MainWindow.xaml.cs
@@ -1,28 +1,40 @@
-﻿
-using System;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-
 
 namespace CircleGeneratorWPF
 {
     public partial class MainWindow : Window
     {
-
         private readonly Circle _circle;
-        private double aspect = 1.3;
+        private readonly double _aspect = 1.3;
         private Size SizeCanvas;
+        
         public MainWindow()
         {
+            InitializeComponent();
+
             canvas = new Canvas();
             _circle = new Circle(40, 10, 10);
-            InitializeComponent();
+
+            var size = GetCanvasSize();
+
+            MessageBox.Show($"{size.Item1} {size.Item2}");
+        }
+
+        private (int, int) GetCanvasSize()
+        {
+            var row = DgCircle.RowDefinitions.First(r => r.Name == "CanvasRow");
+            var column = DgCircle.ColumnDefinitions.First(c => c.Name == "CanvasColumn");
+
+            return ((int)row.Height.Value, (int)column.Width.Value);
         }
     
         private void DrawCircle(int pixelSize)
         {
             canvas.Children.Clear();
+            
             foreach (var point in _circle.Points)
             {
                 canvas.Children.Add(point.Rect);
@@ -30,39 +42,44 @@ namespace CircleGeneratorWPF
                 Canvas.SetTop(point.Rect, point.Y * pixelSize - pixelSize / 2);
             }
         }
+        
         protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
         {
-            if (sizeInfo.WidthChanged) this.Width = sizeInfo.NewSize.Height * aspect;
-            else this.Height = sizeInfo.NewSize.Width / aspect;
+            if (sizeInfo.WidthChanged) Width = sizeInfo.NewSize.Height * _aspect;
+            else Height = sizeInfo.NewSize.Width / _aspect;
             ChangePixelSize();
         }
+        
         private void RadiusPreview(object sender, TextCompositionEventArgs e)
         {
             e.Handled = !IsValid(((TextBox)sender).Text + e.Text);
         }
+        
         private void RadiusChanged(object sender, TextChangedEventArgs e)
         {
-            TextBox textBox = (TextBox)sender;
-            int radius = string.IsNullOrEmpty(textBox.Text) ? 1 : int.Parse(textBox.Text);
+            var textBox = (TextBox)sender;
+            var radius = string.IsNullOrEmpty(textBox.Text) ? 1 : int.Parse(textBox.Text);
+            
             _circle.changeRadius(radius);
             ChangePixelSize();
         }
+        
         private void OnChangeCanvas(object sender, SizeChangedEventArgs e)
         {
-
             SizeCanvas = e.NewSize; // not work, metod dont called
-            MessageBox.Show((SizeCanvas.ToString()));
+            MessageBox.Show(SizeCanvas.ToString());
         }
+        
         private void ChangePixelSize()
         {
             //TODO
             //adaptive size
             DrawCircle(1);
         }
+        
         private static bool IsValid(string str)
         {
-            int i;
-            return int.TryParse(str, out i) && i >= 1 && i <= 300;
+            return int.TryParse(str, out var i) && i >= 1 && i <= 300;
         }
     }
 }
